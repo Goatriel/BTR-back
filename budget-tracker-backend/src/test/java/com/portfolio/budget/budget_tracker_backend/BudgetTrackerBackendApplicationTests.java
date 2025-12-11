fc2b5039d7b8efa3309c3b777ff6084eb6f13aed
VINCENT Gabriel <vincentgab78@gmail.com>
2025-12-11 10:41:21 +0100

    Initial Spring Boot setup

---
+++ budget-tracker-backend/src/test/java/com/portfolio/budget/budget_tracker_backend/BudgetTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.budget.budget_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
